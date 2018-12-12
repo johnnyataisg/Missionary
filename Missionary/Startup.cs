c17bd5777c1653ce2a1d2758c43a807cdda21ed1
johnnyataisg <42900658+johnnyataisg@users.noreply.github.com>
2018-12-12 16:21:07 -0700

    Add project files.

---
+++ Missionary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Missionary.Startup))]
+namespace Missionary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
